1ea87ba54afca8ae7fe345ff07b09f642efb087b
Guilherme-Software <guilherme.lima.programador@gmail.com>
2025-06-15 20:13:03 -0300

    selected_Date

--- main/__init__.py
+++ main/__init__.py
@@ -1,22 +1,23 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, request, jsonify
 from main.data import sales_per_month, sales_per_seller, best_selling_products
 from main.config import app
 
 
-@app.route("/api/sales")
+@app.route("/api/sales/monthly", methods=["GET", "POST"])
 def get_sales():
-    sales = sales_per_month(2025)
+    if request.method == "GET":
+        years = request.args
+    sales = sales_per_month([2024, 2025], [5, 7, 8])
     return jsonify(sales)
 
-@app.route("/api/sellers")
+@app.route("/api/sales/seller")
 def get_sellers():
     sellers = sales_per_seller(2025)
     return jsonify(sellers)
 
-@app.route("/api/products")
+@app.route("/api/sales/product")
 def get_products():
-    products = best_selling_products(2024)
+    products = best_selling_products(2024, 2025)
     return jsonify(products)
 
 app
--- main/data.py
+++ main/data.py
@@ -3,40 +3,61 @@ import pandas as pd
 # read the csv.
 df = pd.read_csv("vendas_ultimos_12_meses.csv")
 
-# transforming data into manipulable dates
-df["Data"] = pd.to_datetime(df["Data"])
-df["year"] = df["Data"].dt.year
-df["month"] = df["Data"].dt.month
+# transforming data into manipulable dates (datetime).
+def preprocess_data():
+    df["Data"] = pd.to_datetime(df["Data"])
+    df["year"] = df["Data"].dt.year
+    df["month"] = df["Data"].dt.month
+
+    return df
+
+# the date the user was selected
+def selected_date(df, years=None, months=None):
+
+    # transform the selected years and months into an array.
+    # if it is already an array, do nothing.
+    if years is not None and not isinstance(years, list):
+        years = [years]
+
+    if months is not None and not isinstance(months, list):
+        months = [months]
+
+
+    # makes pandas read all selected years and months.
+    if years:
+        df = df[df["year"].isin(years)]
+
+    if months:
+        df = df[df["month"].isin(months)]
+
+    return df
+
 
 # sales by year and selected months.
-def sales_per_month(year, months=None):
-
-    df_year = df[df["year"] == year]
-    
-    # if any month was selected, return only the selected months.
-    if months:
-        df_year = df_year[df_year["month"].isin(months)]
+def sales_per_month(years=None, months=None):
+    df = preprocess_data()
+    df = selected_date(df, years, months)
 
     # add up how much was sold each month
-    result = df_year.groupby("month")["Valor (R$)"].sum()
+    result = df.groupby("month")["Valor (R$)"].sum()
 
     # return the result as a dict
     return result.to_dict()
 
 
-def sales_per_seller(year):
+def sales_per_seller(years=None, months=None):
+    df = preprocess_data()
+    df = selected_date(df, years, months)
 
-    df_year = df[df["year"] == year]
-
-    result = df_year.groupby("Vendedor")["Valor (R$)"].sum()
+    result = df.groupby("Vendedor")["Valor (R$)"].sum()
 
     return result.to_dict()
 
 
-def best_selling_products(year):
+def best_selling_products(years=None, months=None):
+    df = preprocess_data()
+    df = selected_date(df, years, months)
 
-   df_year = df[df["year"] == year]
+    result = df.groupby("Produto")["Valor (R$)"].count()
 
-   result = df_year.groupby("Produto")["Valor (R$)"].count()
-
-   return result.to_dict()
+    return result.to_dict()
